59367e4fc43b8f9e8d1c49f51ac22a1c8279f34d
PetkoPopov <poetpoet2014@mail.ru>
2021-01-26 23:38:11 +0200

    най накрая змията се движи толкова лесно пък аз толкова трудно се опитах :)

--- circle.js
+++ circle.js
@@ -31,51 +31,123 @@ var i = 0;
 var j = 30;
 var a = 10;
 var b = 10;
+
+var snake = {
+    position:
+    [
+    { hor: 10, ver: 30 },
+    { hor: 20, ver: 30 },
+    { hor: 30, ver: 30 },
+    { hor: 40, ver: 30},
+    { hor: 40, ver: 20},
+    {hor: 40, ver: 10},
+    {hor: 40, ver: 0},
+    {hor:50,ver:0},
+    {hor:60,ver:0},
+    {hor:70,ver:0}
+],
+direction:"right"
+}
+
 /////////////////////////////////
 ///////////////////////
 function moveSqr() {
 
     createApple();
-    let sizeX=10;
+    showSnake();
     window.addEventListener('keydown', (aa) => {
         canvas.fillStyle = "#45";
-        
-        let iHlp = i;
-        let jHlp = j;
+
         if (aa.key == "ArrowLeft") {
 
-            if (i == 0) { i = 300 }
-            i -= 2;
+            
+            // изчисляваме пътя
+            let path = snake.position[snake.position.length-1];
+        
+            
+            let newX = path.hor - 10;
+            let newY = path.ver;
+            
+            let newPath = { hor: newX, ver: newY };
+        
+            snake.position.push(newPath);
+            
+            showfirstSqr(newX,newY);//    показваме новата кутия
+             
+            // махаме последна кутийка
+            let eraseLast=snake.position[0]
+            
+            clearLastSqr(eraseLast.hor,eraseLast.ver);
+            
+            snake.position.splice(0,1);
+
         } else if (aa.key == "ArrowRight") {
-            i += 2;
-            if (i == 300) { i = 0 }
-
-
+            // изчисляваме пътя
+            let path = snake.position[snake.position.length-1];
+            let newX = path.hor + 10;
+            let newY = path.ver;
+            let newPath = { hor: newX, ver: newY };
+            snake.position.push(newPath);
+            
+            showfirstSqr(newX,newY);//    показваме новата кутия
+             
+            // махаме последна кутийка
+               let eraseLast=snake.position[0]
+               clearLastSqr(eraseLast.hor , eraseLast.ver);
+                 snake.position.splice(0,1);
+           
         } else if (aa.key == "ArrowUp") {
-            j -= 2;
+             // изчисляваме пътя
+             let path = snake.position[snake.position.length - 1];
+             let newX = path.hor ;
+             let newY = path.ver - 10;
+             let newPath = { hor: newX, ver: newY };
+             snake.position.push(newPath);
+             
+             showfirstSqr(newX,newY);//    показваме новата кутия
+              
+             // махаме последна кутийка
+                let eraseLast=snake.position[0];
+                clearLastSqr(eraseLast.hor , eraseLast.ver);
+                  snake.position.splice(0,1);
         } else if (aa.key == "ArrowDown") {
-            j += 2;
+          
+                // изчисляваме пътя
+                let path = snake.position[snake.position.length-1];
+                let newX = path.hor ;
+                let newY = path.ver + 10 ;
+                let newPath = { hor: newX, ver: newY };
+                snake.position.push(newPath);
+                
+                showfirstSqr(newX,newY);//    показваме новата кутия
+                 
+                // махаме последна кутийка
+                   let eraseLast=snake.position[0]
+                   
+                   clearLastSqr(eraseLast.hor , eraseLast.ver);
+                   
+                     snake.position.splice(0,1);
         }
 
-        console.log(i, j, a, b);
-        if(catchApple(a,b,i,j)){
+        // console.log(i, j, a, b);
+        if (catchApple(a, b, i, j)) {
+            snake++;
             console.log("catched");
             createApple();
-            sizeX+=10;
+
         }
-        canvas.clearRect(iHlp, jHlp, sizeX, 10);
-        canvas.fillRect(i, j, sizeX, 10);
-        
+
+
+
     })
 }
-var snake = [];
 
 function createApple() {
     a = Math.floor(Math.random() * 300);
     b = Math.floor(Math.random() * 150);
 
     canvas.fillStyle = "#446599";
-    var sqr = canvas.fillRect(a, b, 10, 10);
+    var sqr = canvas.fillRect(a, b, 5, 5);
     return sqr;
 }
 
@@ -83,11 +155,30 @@ function createApple() {
 //////////////
 
 function catchApple(a, b, i, j) {
-    console.log(a, b, i, j)
+    // console.log(a, b, i, j)
     if ((a > i && a < i + 10) && (b > j && b < j + 10)) {
         console.log('catched');
         return true
     }
     return false;
 }
+////////////////
+function showfirstSqr(a, b) {
+    canvas.fillStyle = "#199";
+    canvas.fillRect(a, b, 10, 10);
+}
+///////////////////
+function clearLastSqr(lastA, lastB) {
+    canvas.clearRect(lastA, lastB, 10, 10);
 
+}
+//////////////////////////////
+
+function showSnake() {
+    // console.log(i,j,a,b);
+
+    for (x of snake.position) {
+        showfirstSqr(x.hor, x.ver);
+    }
+
+}
