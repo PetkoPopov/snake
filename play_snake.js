7d2bcd6c2ecd0340ca5eabc70d6304d6d237bc0f
pipjivko <pipjivko@gmail.com>
2021-12-17 23:18:45 +0200

    вече змията се движи равномерно

--- play_snake.js
+++ play_snake.js
@@ -1,7 +1,7 @@
 var score = 0;
 var myTime= new Date()
 var myStartingTime=myTime.getTime();
-console.log(myStartingTime);
+console.log('starting timestamp',myStartingTime);
 var scoreValue = document.getElementById('score');
 scoreValue.innerHTML=score;
 
@@ -22,16 +22,16 @@ document.getElementById('score').innerHTML=score;
 }
 var canvas = document.getElementById('myCanvas').getContext("2d");
 var stopSnakeAuto = true;
-var stopAutoBtn = document.getElementById("StopAuto");
+var circle = document.getElementById("StopAuto");
 
-stopAutoBtn.addEventListener('click', () => {
-    if (stopAutoBtn.textContent == "StopAuto/Pause") {
+circle.addEventListener('click', () => {
+    if (circle.textContent == "StopAuto/Pause") {
 
         stopSnakeAuto = false;
-        stopAutoBtn.textContent = "StartAuto/Game";
+        circle.textContent = "StartAuto/Game";
     } else {
         stopSnakeAuto = true;
-        stopAutoBtn.textContent = "StopAuto/Pause";
+        circle.textContent = "StopAuto/Pause";
     }
 })
 
@@ -49,9 +49,6 @@ buttonStop.addEventListener('click', () => {
     }
 
 });
-
-//////////////////////////////////////////////////////
-/////////////////////////////////////////////////////
 var step = 10;
 var a = 10;
 var b = 10;
@@ -72,81 +69,111 @@ var snake = {
         ],
     direction: "right"
 }
-
-
 var arrowKeyLeft=true
 var arrowKeyRight=true
 var arrowKeyUp = true
 var arrowKeyDown = true
+//////////////////////
+var btnArrowLeft='up'
+var btnArrowRight='up'
+var btnArrowUp = 'up'
+var btnArrowDown = 'up'
+/////////////////////////////
 
-// const reduceSpeedRight
-// const reduceSpeedLeft
-// const reduceSpeedUp
-// const reduceSpeedDown
+
+var isCalledLeft = false
+var isCalledRight = false
+var isCalledUp = false
+var isCalledDown = false
+
 /////////////////////////////////
-///////////////////////
-function moveSqr() {
+var stopSnakeLeft
+var stopSnakeRight
+var stopSnakeUp
+var stopSnakeDown
+///////////////////
+///////////////////
 
+
+
+
+
+
+////
+function moveSqr() {
+    
+    
     createApple();
     showSnake();
-
-    
 }
+    
 
 window.addEventListener('keyup',function(btn){
     if(btn.key =="ArrowLeft"){
+        clearInterval(stopSnakeLeft)
+        isCalledLeft = false
+        btnArrowLeft='up'
         arrowKeyLeft=false
-    
     }
     if(btn.key =="ArrowRight"){
+        
+        clearInterval(stopSnakeRight)
+        isCalledRight = false
+        btnArrowRight='up'
         arrowKeyRight=false
     }
     if(btn.key =="ArrowUp"){
+        
+        clearInterval(stopSnakeUp)
+        isCalledUp = false
+        btnArrowUp='up'
         arrowKeyUp=false
-    
     }
     if(btn.key =="ArrowDown"){
-        arrowKeyDown=false
         
+        clearInterval(stopSnakeDown)
+        isCalledDown=false
+        btnArrowDown='up'
+        // console.log(btnArrowDown)
+        arrowKeyDown=false
     }
     // console.log(arrowKeyDown,arrowKeyLeft,arrowKeyRight,arrowKeyUp)
 })
 
 // console.log(arrowKeyDown,arrowKeyLeft,arrowKeyRight,arrowKeyUp)
-    window.addEventListener('keydown', (aa) => {
+    window.addEventListener('keydown', (btn) => {
         canvas.fillStyle = "#45";
 
-        if (aa.key == "ArrowLeft" || arrowKeyLeft ) {
-           arrowKeyLeft=true
-            snake.direction = 'left';
-            moveLeft()
-        }
+        if (btn.key == "ArrowLeft" ) {
+           //arrowKeyLeft=true
+        //    btnArrowLeft="down"
+        //    snake.direction = 'left';
+            moveLeft();
             
-        if (aa.key == "ArrowRight" || arrowKeyRight) {
-            arrowKeyRight=true
-           
-        //    clearInterval(reduceSpeedRight)  
-            snake.direction = 'right'
+        }
+        if (btn.key == "ArrowRight" ) {
+            //arrowKeyRight=true
+            // btnArrowRight='down'
+            // snake.direction = 'right'
             moveRight();
 
         }
-        if (aa.key == "ArrowUp" || arrowKeyUp ) {
-            arrowKeyUp=true
-            snake.direction = 'up';
-            // clearInterval(reduceSpeedUp)
+        if (btn.key == "ArrowUp"  ) {
+            //arrowKeyUp=true
+            // btnArrowUp='down'
+            // snake.direction = 'up';
             moveUp();
         }
-        if (aa.key == "ArrowDown" || arrowKeyDown) {
-            arrowKeyDown=true
-            snake.direction = 'down';
-        
-    //   clearInterval(reduceSpeedDown)
+        if (btn.key == "ArrowDown" ) {
+            //arrowKeyDown=true
+            // btnArrowDown='down'
+            // snake.direction = 'down';
+            // console.log('btn_down' , btnArrowDown )
             moveDown();
         }
 
         catchApple();
-    }
-    )
+    })
 
 
 function createApple() {
@@ -212,11 +239,13 @@ function showfirstSqr(a, b, color = '',text='') {
 ///////////////////
 function clearLastSqr(lastA, lastB) {
     canvas.clearRect(lastA, lastB, 10, 10);
+
 }
 //////////////////////////////
 
 function showSnake() {
-    
+    // console.log(i,j,a,b);
+
     for (x of snake.position) {
         showfirstSqr(x.hor, x.ver);
     }
@@ -225,7 +254,32 @@ function showSnake() {
 /////////////////////
 function moveRight() {
 
-    
+
+    if(isCalledRight == true ){return true}
+     else{isCalledRight = true }
+
+     if(stopSnakeAuto === true ){
+        stopSnakeRight = setInterval(()=>{
+
+            let path = snake.position[snake.position.length - 1];
+            let newX = path.hor + step;
+            if(newX>300){
+                newX = 0;
+            }
+            let newY = path.ver;
+            let newPath = { hor: newX, ver: newY };
+            snake.position.push(newPath);
+        
+            showfirstSqr(newX, newY, '#355');//    показваме новата кутия
+        
+            // махаме последна кутийка
+            let eraseLast = snake.position[0]
+            clearLastSqr(eraseLast.hor, eraseLast.ver);
+            snake.position.splice(0, 1);
+            showSnake();
+            
+         },speed)
+     }else{
     let path = snake.position[snake.position.length - 1];
     let newX = path.hor + step;
     if(newX>300){
@@ -242,11 +296,35 @@ function moveRight() {
     clearLastSqr(eraseLast.hor, eraseLast.ver);
     snake.position.splice(0, 1);
     showSnake();
-
+  }
 }
+
+
 /////////////////////
 function moveLeft() {
-    
+    if(isCalledLeft == true ){return true}
+    else{isCalledLeft = true }
+    if(stopSnakeAuto === true ){
+        stopSnakeLeft = setInterval(()=>{
+
+            let path = snake.position[snake.position.length - 1];
+            let newX = path.hor - step;
+            if(newX < 0 ){
+                newX = 300;
+            }
+            let newY = path.ver;
+            let newPath = { hor: newX, ver: newY };
+            snake.position.push(newPath);
+        
+            showfirstSqr(newX, newY, 'olive');//    показваме новата кутия
+        
+            // махаме последна кутийка
+            let eraseLast = snake.position[0]
+            clearLastSqr(eraseLast.hor, eraseLast.ver);
+            snake.position.splice(0, 1);
+            showSnake();
+        },speed)
+        }else{
     let path = snake.position[snake.position.length - 1];
     let newX = path.hor - step;
     if(newX < 0 ){
@@ -263,11 +341,19 @@ function moveLeft() {
     clearLastSqr(eraseLast.hor, eraseLast.ver);
     snake.position.splice(0, 1);
     showSnake();
-
+ }
 }
+///////////////////////////////
+
 /////////////////////
 function moveUp() {
+   
+    if(isCalledUp === true){return true}
+    else{isCalledUp = true }
 
+    if(stopSnakeAuto === true ){
+        stopSnakeUp = setInterval(()=>{
+     
     let path = snake.position[snake.position.length - 1];
     let newX = path.hor;
     let newY = path.ver - step;
@@ -283,12 +369,38 @@ function moveUp() {
     let eraseLast = snake.position[0]
     clearLastSqr(eraseLast.hor, eraseLast.ver);
     snake.position.splice(0, 1);
-    showSnake();
+    showSnake();       
+        },speed)
 
-}
+    }else{
+        let path = snake.position[snake.position.length - 1];
+        let newX = path.hor;
+        let newY = path.ver - step;
+        if(newY < 0 ){
+            newY = 140 ;
+        }
+        let newPath = { hor: newX, ver: newY };
+        snake.position.push(newPath);
+    
+        showfirstSqr(newX, newY, "olive");//    показваме новата кутия
+    
+        // махаме последна кутийка
+        let eraseLast = snake.position[0]
+        clearLastSqr(eraseLast.hor, eraseLast.ver);
+        snake.position.splice(0, 1);
+        showSnake();
+    }
+  }
+
 /////////////////////
 function moveDown() {
-
+    // console.log("IN MOVE DOWN " , isCalledDown)
+    if(isCalledDown === true ){return 0}
+    else{isCalledDown = true }
+// console.log("AfTER IF " , isCalledDown)
+    if(stopSnakeAuto === true ){
+        stopSnakeDown = setInterval(()=>{
+            
     let path = snake.position[snake.position.length - 1];
     let newX = path.hor;
     let newY = path.ver + step;
@@ -305,10 +417,27 @@ function moveDown() {
     clearLastSqr(eraseLast.hor, eraseLast.ver);
     snake.position.splice(0, 1);
     showSnake();
-    
+        },speed)
+    }else{
+    let path = snake.position[snake.position.length - 1];
+    let newX = path.hor;
+    let newY = path.ver + step;
+    if( newY > 140){
+        newY = 0;
+    }
+    let newPath = { hor: newX, ver: newY };
+    snake.position.push(newPath);
 
+    showfirstSqr(newX, newY, 'olive');//    показваме новата кутия
+
+    // махаме последна кутийка
+    let eraseLast = snake.position[0]
+    clearLastSqr(eraseLast.hor, eraseLast.ver);
+    snake.position.splice(0, 1);
+    showSnake();
+  }
 }
-///////////////////////////
+////////////////////////
 ///////////////////////
 function addToSnake() {
 
@@ -317,6 +446,7 @@ function addToSnake() {
     add.hor = coor.hor--;
     add.ver = coor.ver;
     snake.position.unshift(add);
+    
 }
 
 
